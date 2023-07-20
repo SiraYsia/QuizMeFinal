728b8a78ef7d9d589cac705d0955a71210051118
Iman Kiio <imank2390@gmail.com>
2023-07-20 23:26:43 +0000

    New tests

--- tests/test.py
+++ tests/test.py
@@ -18,6 +18,14 @@ class BasicTests(unittest.TestCase):
     def test_main_page(self):
         response = self.app.get('/', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
+    
+    def test_new_flash(self):
+        response = self.app.get('/new_flash', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+    
+    def test_get_started(self):
+        response = self.app.get('/getstarted', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
 
 if __name__ == "__main__":
     unittest.main()
